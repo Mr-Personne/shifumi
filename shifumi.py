3d4b3c1bc5b4c39a63c3274f57fd6bdabcf7873b
Mr-Personne <h.sacha07@gmail.com>
2020-10-03 12:03:42 +0000

    added scoring (to keep track because the computer was winning so often that i would swear he cheats... even though he shouldnt)

--- shifumi.py
+++ shifumi.py
@@ -3,6 +3,8 @@ import random
 print('shifumi start')
 
 hands = ['rock', 'paper', 'scissors']
+playerScore = 0
+computerScore = 0
 #print(hands[2])
 
 playagain = 'y'
@@ -26,32 +28,52 @@ while playagain == 'y':
     print('player hand : ', playerHand, ' vs computer ', randHand)
     # compare hands and declare winner
     if playerHand == 'rock' and randHand == 'paper':
-        print('Computer wins with paper over rock')
+        print('Computer wins with paper over rock :,(')
+        computerScore = computerScore + 1
         playagain = input('Playagain? y/n ==> ')
 
     elif playerHand == 'rock' and randHand == 'scissors':
-        print('Player wins with rock against scissors')
+        print('Player wins with rock against scissors :D')
+        playerScore = playerScore + 1
         playagain = input('Playagain? y/n ==> ')
 
     elif playerHand == 'paper' and randHand == 'scissors':
-        print('Computer wins with scissors against paper')
+        print('Computer wins with scissors against paper :,(')
+        computerScore = computerScore + 1
         playagain = input('Playagain? y/n ==> ')
 
     elif playerHand == 'paper' and randHand == 'rock':
-        print('Player wins with paper against rock')
+        print('Player wins with paper against rock :D')
+        playerScore = playerScore + 1
         playagain = input('Playagain? y/n ==> ')
 
     elif playerHand == 'scissors' and randHand == 'rock':
-        print('Computer wins with rock against scissors')
+        print('Computer wins with rock against scissors :,(')
+        computerScore = computerScore + 1
         playagain = input('Playagain? y/n ==> ')
 
     elif playerHand == 'scissors' and randHand == 'paper':
-        print('Player wins with scissors against paper')
+        print('Player wins with scissors against paper :D')
+        playerScore = playerScore + 1
         playagain = input('Playagain? y/n ==> ')
 
     else:
         print('Same hands, choose again...')
         playagain = 'y'
+
+
+    #score display...and comments...
+    print('Player Score = ', playerScore, " / Computer Score = ", computerScore)
+    computerWinRate = computerScore - playerScore
+
+    if computerWinRate == 5:
+        print("-- it's okay, it happens, I'm just lucky I guess --")
+    elif computerWinRate == 10:
+        print('-- um, waw, look at that... guess I must be really lucky today huh... --')
+    elif computerWinRate == 15:
+        print("-- ...I swear I'm not cheating ^^' ... --")
+    elif computerWinRate > 15:
+        print('-- ... --')
 
     
 else:
