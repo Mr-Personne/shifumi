b1721e507e18d432d5687ca72feff69ac10b00b8
Mr-Personne <h.sacha07@gmail.com>
2020-10-03 11:26:11 +0000

    player hand input and while loop check if correct input

--- shifumi.py
+++ shifumi.py
@@ -8,8 +8,22 @@ hands = ['rock', 'paper', 'scissors']
 playagain = 'y'
 
 while playagain == 'y':
-    randindex = random.randint(0, 2)
-    print('randindex = ', randindex)
+    # generate random computer hand
+    randIndex = random.randint(0, 2)
+    randHand = hands[randIndex]
+    # print('randgen = ', randHand)
+
+
+    # ask for player hand input and check if it's a correct hand to use
+    playerHand = input('What hand will you play? ==> ')
+    while playerHand not in hands:
+        playerHand = input('Unknown hand, please choose between "rock", "paper" or "scissors" ==> ')
+        # print('playerHand = ', playerHand)
+    else:
+        pass
+
+    print('player hand : ', playerHand, ' vs computer ', randHand)
+
     playagain = input('Playagain? y/n ==> ')
 else:
     print('goodbye')
