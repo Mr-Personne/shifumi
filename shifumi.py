52b7bb96839a5dbb7ded5e6bfadf358b2d499003
Mr-Personne <h.sacha07@gmail.com>
2020-10-03 11:00:46 +0000

    base while loop (list, randindex gen, playagain prompt)

--- shifumi.py
+++ shifumi.py
@@ -2,3 +2,14 @@ import random
 
 print('shifumi start')
 
+hands = ['rock', 'paper', 'scissors']
+#print(hands[2])
+
+playagain = 'y'
+
+while playagain == 'y':
+    randindex = random.randint(0, 2)
+    print('randindex = ', randindex)
+    playagain = input('Playagain? y/n ==> ')
+else:
+    print('goodbye')
